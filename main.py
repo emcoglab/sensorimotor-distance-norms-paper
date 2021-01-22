d38976ba148af47d05173e0fe2647836c290e426
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2021-01-22 08:57:37 +0000

    Logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from logging import getLogger, basicConfig, INFO
 from pathlib import Path
 from typing import Tuple, Optional, Callable
 
@@ -15,6 +16,20 @@ from sensorimotor_norms.exceptions import WordNotInNormsError
 from sensorimotor_norms.sensorimotor_norms import SensorimotorNorms
 
 
+_logger = getLogger(__name__)
+
+
+def load_nelson_data():
+    with Path(Path(__file__).parent, "data", "Nelson_AppendixB.csv").open() as nelson_file:
+        nelson = read_csv(nelson_file, skip_blank_lines=True, header=0)
+    nelson["Targets"] = nelson["Targets"].str.lower()
+    nelson["Part of Speech"] = nelson["Part of Speech"].str.lower()
+    return nelson
+
+
+NELSON = load_nelson_data()
+
+
 def load_jcn_data() -> DataFrame:
     jcn_path = Path(Path(__file__).parent, "data", "Maki-BRMIC-2004", "usfjcnlsa.csv")
     with open(jcn_path) as jcn_file:
@@ -23,15 +38,29 @@ def load_jcn_data() -> DataFrame:
     return jcn_data
 
 
-def add_extra_predictors(dataset: DataFrame, word_key_cols: Tuple[str, str], pos: Optional[str] = None):
-    add_sensorimotor_predictor(dataset, word_key_cols)
-    if pos is not None:
-        add_jcn_predictor(dataset, word_key_cols, pos)
+def get_nelson_pos(word: str) -> Optional[str]:
+    nelson_to_wordnet = {
+        "n": NOUN,
+        "v": VERB,
+        "aj": ADJ,
+        "ad": ADV,
+    }
+    try:
+        pos = NELSON[NELSON["Targets"] == word]["Part of Speech"].iloc[0]
+    except IndexError:
+        # Word not found
+        return None
+    try:
+        return nelson_to_wordnet[pos]
+    except KeyError:
+        # These are the only POSs we support
+        return None
 
 
 def add_sensorimotor_predictor(dataset: DataFrame, word_key_cols: Tuple[str, str]):
     predictor_name = "Sensorimotor distance"
     if predictor_name in dataset.columns:
+        _logger.info("Predictor already exists, skipping")
         return
     key_col_1, key_col_2 = word_key_cols
     sn = SensorimotorNorms()
@@ -53,39 +82,10 @@ def add_sensorimotor_predictor(dataset: DataFrame, word_key_cols: Tuple[str, str
     dataset[predictor_name] = dataset.apply(calc_sensorimotor_distance, axis=1)
 
 
-def load_nelson_data():
-    with Path(Path(__file__).parent, "data", "Nelson_AppendixB.csv").open() as nelson_file:
-        nelson = read_csv(nelson_file, skip_blank_lines=True, header=0)
-    nelson["Targets"] = nelson["Targets"].str.lower()
-    nelson["Part of Speech"] = nelson["Part of Speech"].str.lower()
-    return nelson
-
-
-nelson = load_nelson_data()
-
-
-def get_nelson_pos(word: str) -> Optional[str]:
-    nelson_to_wordnet = {
-        "n": NOUN,
-        "v": VERB,
-        "aj": ADJ,
-        "ad": ADV,
-    }
-    try:
-        pos = nelson[nelson["Targets"] == word]["Part of Speech"].iloc[0]
-    except IndexError:
-        # Word not found
-        return None
-    try:
-        return nelson_to_wordnet[pos]
-    except KeyError:
-        # These are the only POSs we support
-        return None
-
-
 def add_jcn_predictor(dataset: DataFrame, word_key_cols: Tuple[str, str], pos: str):
     predictor_name = "JCN distance"
     if predictor_name in dataset.columns:
+        _logger.info("Predictor already exists, skipping")
         return
     key_col_1, key_col_2 = word_key_cols
 
@@ -139,19 +139,34 @@ def add_jcn_predictor(dataset: DataFrame, word_key_cols: Tuple[str, str], pos: s
 
 
 def process(out_dir: str, out_file_name: str, load_from_source: Callable[[], DataFrame], word_key_cols: Tuple[str, str], pos: Optional[str]):
+    _logger.info(out_file_name)
+
     data_path = Path(out_dir, out_file_name)
     data: DataFrame
     if not data_path.exists():
+        _logger.info("Loading from source")
         data = load_from_source()
     else:
+        _logger.info("Loading previously saved file")
         with data_path.open(mode="r") as data_file:
             data = read_csv(data_file, header=0, index_col=None)
-    add_extra_predictors(data, word_key_cols=word_key_cols, pos=pos)
+
+    _logger.info("Adding sensorimotor predictor")
+    add_sensorimotor_predictor(data, word_key_cols)
+    _logger.info("Saving")
     with data_path.open(mode="w") as out_file:
         data.to_csv(out_file, header=True, index=False)
+    if pos is not None:
+        _logger.info("Adding WordNet JCN predictor")
+        add_jcn_predictor(data, word_key_cols, pos)
+        _logger.info("Saving")
+        with data_path.open(mode="w") as out_file:
+            data.to_csv(out_file, header=True, index=False)
 
 
 if __name__ == '__main__':
+
+    basicConfig(format='%(asctime)s | %(levelname)s | %(module)s | %(message)s', datefmt="%Y-%m-%d %H:%M:%S", level=INFO)
 
     out_dir = Path("/Users/caiwingfield/Desktop/")
 
@@ -164,3 +179,5 @@ if __name__ == '__main__':
     process(out_dir, "jcn.csv", lambda: load_jcn_data(), ("CUE", "TARGET"), "nelson")
     process(out_dir, "swow_r1.csv", lambda: SmallWorldOfWords(responses_type=SmallWorldOfWords.ResponsesType.R1).associations_to_dataframe(), (WordAssociationTest.TestColumn.word_1, WordAssociationTest.TestColumn.word_2), "n")
     process(out_dir, "swow_r123.csv", lambda: SmallWorldOfWords(responses_type=SmallWorldOfWords.ResponsesType.R123).associations_to_dataframe(), (WordAssociationTest.TestColumn.word_1, WordAssociationTest.TestColumn.word_2), "n")
+
+    _logger.info("Done!")
