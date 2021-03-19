0551ce7d9d2b8549e8cb708ccc44578d9ab68ded
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2021-03-19 11:31:32 +0000

    Fix binning fence post error

--- visualisation/distributions.py
+++ visualisation/distributions.py
@@ -14,17 +14,24 @@ from sensorimotor_norms.sensorimotor_norms import SensorimotorNorms
 sn = SensorimotorNorms()
 
 
-def bin_distances(bins, distance_type, n_bins):
+def bin_distances(bins, distance_type):
     """
-    We don't want to get the entire 40k-by-40k distance matrix into memory at once, so instead we can pre-define the
-    bins for the histograms, and accumulate the totals a bit at a time.
+    Get all pairwise distances from the model and graph a histogram distribution using the specified bins.
 
-    :param bins:
+    :param bins: array of bin inclusive lower-bounds plus the inclusive upper bound of the last bin (e.g. from linspace)
     :param distance_type:
-    :param n_bins:
     :return:
     """
-    binned_distances = zeros(n_bins - 1, dtype=int)
+
+    n_bins = len(bins) - 1
+
+    # We don't want to get the entire 40k-by-40k distance matrix into memory at once, so instead we can pre-define the
+    # bins for the histograms, and accumulate the totals a bit at a time.
+
+    binned_distances = zeros(n_bins,
+                             # floats to avoid broadcasting errors on divides below.
+                             # We'll cast to int later
+                             dtype=float)
     all_words = list(sn.iter_words())
     for i, word in enumerate(all_words):
         if i % 1_000 == 0:
@@ -40,6 +47,7 @@ def bin_distances(bins, distance_type, n_bins):
 
         htemp, _ = histogram(distances, bins)
         binned_distances += htemp
+
     # We've double-counted many of the distances by matching word X with all words (including Y) and word Y with all
     # words (including X). However we haven't double-counted the diagonal (each word X was matched with all words,
     # including X, but only once).
@@ -54,7 +62,9 @@ def bin_distances(bins, distance_type, n_bins):
         # And all of the identity pairs
         + n_identity_pairs
     )
-    return binned_distances
+
+    assert (binned_distances == binned_distances.astype(int)).all()
+    return binned_distances.astype(int)
 
 
 def style_histplot(ax: Axes, xlim: Tuple[float, float]):
@@ -87,19 +97,27 @@ def graph_distance_distribution(distance_type: DistanceType, n_bins: int, locati
         max_distance = distance(repeat(sn.rating_min, 11), repeat(sn.rating_max, 11), distance_type=distance_type)
     logger.info(f"Max theoretical pairwise {distance_type.name} distance between concepts: {max_distance}")
 
-    bins = linspace(start=min_distance, stop=max_distance, num=n_bins)
+    # [:-1] determine the inclusive lower-bounds of each bin.
+    # [-1] determines the inclusive upper bound of the last bin
+    bins = linspace(start=min_distance, stop=max_distance,
+                    # That's why we need the +1 here
+                    # E.g. 100 bins requires 101 specified bounds
+                    num=n_bins + 1)
 
     if distribution_save_path.exists() and not overwrite:
         logger.warning(f"{distribution_save_path} exists, skipping")
         with distribution_save_path.open("r") as distribution_file:
             binned_distances = loadtxt(distribution_file)
     else:
-        binned_distances = bin_distances(bins, distance_type, n_bins)
+        binned_distances = bin_distances(bins, distance_type)
         with distribution_save_path.open("w") as distribution_file:
             savetxt(distribution_file, binned_distances)
 
     fig, ax = pyplot.subplots(tight_layout=True)
-    ax.hist(bins[:-1], weights=binned_distances, bins=bins)
+    ax.hist(
+        # Everything gets set to the left-hand edge
+        bins[:-1],
+        weights=binned_distances, bins=bins)
 
     style_histplot(ax, xlim=(min_distance, max_distance))
 
@@ -108,7 +126,11 @@ def graph_distance_distribution(distance_type: DistanceType, n_bins: int, locati
 
 
 def graph_distance_distributions(n_bins: int, location: Path, overwrite: bool):
-    graph_distance_distribution(distance_type=DistanceType.cosine, n_bins=n_bins, location=location, overwrite=overwrite)
-    graph_distance_distribution(distance_type=DistanceType.correlation, n_bins=n_bins, location=location, overwrite=overwrite)
-    graph_distance_distribution(distance_type=DistanceType.Minkowski3, n_bins=n_bins, location=location, overwrite=overwrite)
-    graph_distance_distribution(distance_type=DistanceType.Euclidean, n_bins=n_bins, location=location, overwrite=overwrite)
+    graph_distance_distribution(
+        distance_type=DistanceType.cosine, n_bins=n_bins, location=location, overwrite=overwrite)
+    graph_distance_distribution(
+        distance_type=DistanceType.correlation, n_bins=n_bins, location=location, overwrite=overwrite)
+    graph_distance_distribution(
+        distance_type=DistanceType.Minkowski3, n_bins=n_bins, location=location, overwrite=overwrite)
+    graph_distance_distribution(
+        distance_type=DistanceType.Euclidean, n_bins=n_bins, location=location, overwrite=overwrite)
