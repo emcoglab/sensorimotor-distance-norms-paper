ea988012ab6c58a0956862036dd0a947f07413f0
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2022-02-03 11:57:54 +0000

    scripts for answering reviews

---
+++ closest_furthest.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from numpy import inf
+
+all_ds = Path("/Users/caiwingfield/Box Sync/LANGBOOT Project/Manuscripts/Draft - Sensorimotor distance norms/Output/")
+
+min_dist = inf
+min_w1, min_w2 = "", ""
+max_dist = -inf
+max_w1, max_w2 = "", ""
+
+with all_ds.open("r") as f:
+    f.readline()  # header
+    for i, line in enumerate(f):
+        w1, w2, d = line.split(",")
+        d = float(d)
+        if (w1 != w2) and (d < min_dist):
+            min_dist = d
+            min_w1, min_w2 = w1, w2
+            print(i, w1, w2, d)
+        elif d > max_dist:
+            max_dist = d
+            max_w1, max_w2 = w1, w2
+            print(i, w1, w2, d)
+
+print("---")
+print(min_w1, min_w2, min_dist)
+print(max_w1, max_w2, max_dist)
---
+++ exclusivity_correlation.py
@@ -0,0 +1,33 @@
+from numpy import corrcoef, zeros
+from numpy.random import default_rng, seed
+
+from linguistic_distributional_models.utils.logging import print_progress
+from linguistic_distributional_models.utils.maths import DistanceType, distance
+from sensorimotor_norms.sensorimotor_norms import SensorimotorNorms, DataColNames
+
+sn = SensorimotorNorms(use_breng_translation=False, verbose=True)
+
+n_draws = 10_000
+seed(451)
+rng = default_rng()
+
+all_words = list(sn.iter_words())
+
+random_words = rng.choice(all_words, 2 * n_draws, replace=True)
+w1s = random_words[:n_draws]
+w2s = random_words[n_draws:]
+
+ds = zeros((n_draws, ))
+es = zeros((n_draws, ))
+for i in range(n_draws):
+    w1, w2 = w1s[i], w2s[i]
+    v1, v2 = sn.vector_for_word(w1), sn.vector_for_word(w2)
+    e1, e2 = sn.stat_for_word(w1, DataColNames.exclusivity_sensorimotor), sn.stat_for_word(w2, DataColNames.exclusivity_sensorimotor)
+
+    # For the pair
+    ds[i] = distance(v1, v2, DistanceType.cosine)  # vector distance
+    es[i] = (e1 + e2) / 2  # mean exclusivity
+
+    print_progress(i+1, n_draws)
+
+print(corrcoef(ds, es))
